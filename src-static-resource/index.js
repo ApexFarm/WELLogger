f4aba20acdccd65821b5edc47c97d460af741d18
Jeff Jin <jeff.jianfeng.jin@gmail.com>
2019-01-22 23:55:51 +0800

    feat: print logs from lightening component

---
+++ src-static-resource/index.js
@@ -0,0 +1,19 @@
+var debug = require('./browser.js');
+module.exports = function(logs) {
+  if (logs != null && Array.isArray(logs) && logs.length > 0) {
+    debug.log('==========BEGIN==========');
+    logs.forEach(log => {
+      var nsp = debug(log.nsp);
+      if (log.trc) {
+        if (typeof log.trc === 'string') {
+          nsp(log.tst, '%s\n%s', log.msg, log.trc);
+        } else {
+          nsp(log.tst, '%s\n%o', log.msg, log.trc);
+        }
+      } else {
+        nsp(log.tst, log.msg);
+      }
+    });
+    debug.log('===========END===========');
+  }
+}
--- src/sample/aura/WELSample/WELSampleController.js
+++ src/sample/aura/WELSample/WELSampleController.js
@@ -1,5 +1,30 @@
 ({
-    myAction : function(component, event, helper) {
+    doInit : function(component, event, helper) {
+        var action = component.get("c.doSomeWork");
+        // action.setParams({ firstName : component.get("v.firstName") });
 
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            debugger;
+            if (state === "SUCCESS") {
+                wellogger(response.getReturnValue().logs);
+            }
+            else if (state === "INCOMPLETE") {
+                // do something
+            }
+            else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " +
+                                 errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        });
+
+        $A.enqueueAction(action);
     }
 })
